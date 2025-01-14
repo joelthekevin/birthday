aae62a40354c6cde02b5584fc241d391e566b88c
Joel <92096050+joelthekevin@users.noreply.github.com>
2025-01-14 21:46:31 +0530

    Add files via upload

---
+++ birthday-website/script.js
@@ -0,0 +1,19 @@
+// Open Popup
+function openPopup() {
+  document.getElementById('popup').style.display = 'flex';
+  triggerConfetti();
+}
+
+// Close Popup
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+function anotherSurprise() {
+  document.getElementById('another-surprise').style.display = 'flex';
+}
+
+// Close the "Another Surprise!" modal
+function closeAnotherSurprise() {
+  document.getElementById('another-surprise').style.display = 'none';
+}
